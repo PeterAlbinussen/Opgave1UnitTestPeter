144680d0b3d5fb6050d6e4b23c45f4c467805fa5
Peter Albinussen <pete47m0@edu.easj.dk>
2021-09-29 10:08:28 +0200

    Add project files.

---
+++ Opgave1UnitTestPeter/FootballPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Opgave1UnitTestPeter
+{
+    public class FootballPlayer
+    {
+
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _shirtNumber;
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 4) throw new ArgumentException("name length has to be longer than 4 characters");
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// price has to be greater than 0
+        /// </summary>
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("price has to be larger than 0");
+                _price = value;
+            }
+        }
+
+        /// <summary>
+        /// The ShirtNumber has to be inbetween 1 and 100, if its greater the method won't work.
+        /// </summary>
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (value <= 1 || value >= 100) throw new ArgumentException("ShirtNumber has to be between 1 or 100");
+                _shirtNumber = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
+
---
+++ Opgave1UnitTestPeterTests/FootballPlayerTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave1UnitTestPeter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1UnitTestPeter.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        [TestMethod()]
+        public void IdTest()
+        {
+            FootballPlayer fbPlayer = new FootballPlayer();
+            fbPlayer.Id = 1;
+            Assert.AreEqual(1, fbPlayer.Id);
+            //
+        }
+
+        [TestMethod()]
+        public void NameTest()
+        {
+            FootballPlayer fbPlayer = new FootballPlayer();
+            //fbPlayer.Name = "Bo";
+            fbPlayer.Name = "Freddy";
+            Assert.AreEqual("Freddy", fbPlayer.Name);
+            Assert.ThrowsException<ArgumentException>(
+                () => fbPlayer.Name = "Bo");
+
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            FootballPlayer fbplayer = new FootballPlayer();
+            fbplayer.Price = 1000;
+            Assert.AreEqual(1000, fbplayer.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => fbplayer.Price = -1000);
+
+
+        }
+
+        [TestMethod()]
+        public void ShirtNumberTest()
+        {
+            FootballPlayer fbplayer = new FootballPlayer();
+            fbplayer.ShirtNumber = 24;
+            Assert.AreEqual(24, fbplayer.ShirtNumber);
+            Assert.ThrowsException<ArgumentException>(
+                () => fbplayer.ShirtNumber = -10);
+
+        }
+    }
+}
